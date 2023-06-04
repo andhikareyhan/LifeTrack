9bf3048a22eb90313c45b2eca34d598c53caaf8f
Andhika Reyhan Soebroto <67231183+andhikareyhan@users.noreply.github.com>
2023-06-04 17:16:57 +0100

    Create influxdb.py
    Example code to send data to InfluxDB cloud using Python

---
+++ InfluxDB/influxdb.py
@@ -0,0 +1,58 @@
+import influxdb_client, os, time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+token = os.environ.get("INFLUXDB_TOKEN")
+org = "andhikareyhan@gmail.com"
+url = "https://ap-southeast-2-1.aws.cloud2.influxdata.com"
+
+write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+
+bucket="testPython2"
+
+# Define the write api
+write_api = write_client.write_api(write_options=SYNCHRONOUS)
+
+data = {
+  "point1": {
+    "location": "Klamath",
+    "species": "bees",
+    "count": 23,
+  },
+  "point2": {
+    "location": "Portland",
+    "species": "ants",
+    "count": 30,
+  },
+  "point3": {
+    "location": "Klamath",
+    "species": "bees",
+    "count": 28,
+  },
+  "point4": {
+    "location": "Portland",
+    "species": "ants",
+    "count": 32,
+  },
+  "point5": {
+    "location": "Klamath",
+    "species": "bees",
+    "count": 29,
+  },
+  "point6": {
+    "location": "Portland",
+    "species": "ants",
+    "count": 40,
+  },
+}
+
+for key in data:
+  point = (
+    Point("census")
+    .tag("location", data[key]["location"])
+    .field(data[key]["species"], data[key]["count"])
+  )
+  write_api.write(bucket=bucket, org=org, record=point)
+  time.sleep(1) # separate points by 1 second
+
+print("Complete. Return to the InfluxDB UI.")
